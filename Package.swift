9a275e595cc43831eb02924a10cde0adc692304a
Moritz Lang <hi@slashmo.codes>
2020-02-27 08:52:31 +0100

    Change Swift version to 5.1.3

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.1
 import PackageDescription
 
 let package = Package(
-  name: "weasel",
-  targets: [
-    .target(name: "App", dependencies: []),
-    .target(name: "Run", dependencies: ["App"]),
-    .testTarget(name: "AppTests", dependencies: ["App"])
-  ]
+	name: "weasel",
+	targets: [
+		.target(name: "App", dependencies: []),
+		.target(name: "Run", dependencies: ["App"]),
+		.testTarget(name: "AppTests", dependencies: ["App"])
+	]
 )
