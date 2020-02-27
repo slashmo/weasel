2f1ee69333b0fc3d786b42c736f9c7bda6136b5c
Moritz Lang <hi@slashmo.codes>
2020-02-27 20:14:59 +0100

    Add swift-log library 📦

--- Package.swift
+++ Package.swift
@@ -3,9 +3,12 @@ import PackageDescription
 
 let package = Package(
 	name: "weasel",
+	dependencies: [
+		.package(url: "https://github.com/apple/swift-log.git", .upToNextMajor(from: "1.2.0"))
+	],
 	targets: [
-		.target(name: "App", dependencies: []),
-		.target(name: "Run", dependencies: ["App"]),
+		.target(name: "App", dependencies: ["Logging"]),
+		.target(name: "Run", dependencies: ["App", "Logging"]),
 		.testTarget(name: "AppTests", dependencies: ["App"])
 	]
 )
